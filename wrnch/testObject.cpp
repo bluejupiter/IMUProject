3077759be63bc2f45d6f0a02634736f2a2e78ebe
Miles <milesburuma@gmail.com>
2017-01-16 19:07:05 -0500

    Faulty path trace, delayed compared to sphere

--- wrnch/testObject.cpp
+++ wrnch/testObject.cpp
@@ -5,17 +5,44 @@
 
 
 testObject::testObject(GLfloat r, int sl, int st, double x, double y, double z) :
-	radius(r), slices(sl), stacks(st), x(x), y(y), z(z) {
+	radius(r), slices(sl), stacks(st), x(x), y(y), z(z), pointsCount(0), fullTrace(false) {
 	time = Clock::getInstance(); 
 }
 
 void testObject::update() {
 	time->tick(0.01);
-	x = sin(time->getTime());
-	z = cos(time->getTime());
+	x = 2*sin(time->getTime());
+	z = 2*cos(time->getTime());
 
 	glPushMatrix();
+	glRotated(time->getTime()*100, 0, 1, 0); 
 	glTranslated(x, y, z);
 	glutWireSphere(radius, slices, stacks);
 	glPopMatrix();
+
+	updateTrace(x, y, z);
+	drawTrace(); 
+
+}
+
+void testObject::updateTrace(double x, double y, double z) {
+	pointsTrace[pointsCount][0] = x; 
+	pointsTrace[pointsCount][1] = y;
+	pointsTrace[pointsCount][2] = z;
+	pointsCount++; 
+
+	if (pointsCount == 100) {
+		fullTrace = true;
+		pointsCount = 0;
+	}
+}
+
+void testObject::drawTrace() {
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_DOUBLE, 3 * sizeof(double), pointsTrace);
+	if (!fullTrace)
+		glDrawArrays(GL_POINTS, 0, pointsCount);
+	else
+		glDrawArrays(GL_POINTS, 0, 99);
+
 }
--- wrnch/testObject.h
+++ wrnch/testObject.h
@@ -10,8 +10,13 @@ class testObject {
 	double y = 4;
 	double z;
 	Clock * time; 
+	double pointsTrace[100][3];
+	int pointsCount; 
+	bool fullTrace; 
 
 public:
 	testObject(GLfloat r, int sl, int st, double x, double y, double z);
 	void update();
+	void updateTrace(double, double, double); 
+	void drawTrace();
 };

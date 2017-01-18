01c6b9f9b45f12e5a87b31feec06ac35ba8f3d98
Miles <milesburuma@gmail.com>
2017-01-18 16:07:37 -0500

    Fixed bug with orienationEstimator

--- wrnch/OrientationEstimator.h
+++ wrnch/OrientationEstimator.h
@@ -2,24 +2,23 @@
 #include "MockIMU.h";
 #include "Quaternion.h"
 #include <vector>
-using std::vector; 
+using std::vector;
 
 class OrientationEstimator {
 	MockIMU * imu;
 	vector<double> North;          // First row of DCM matrix. "North" in the body frame
 	vector<double> West;			// Second row of DCM matrix. "West" in the body frame
 	vector<double> Zenith;			// Third row of DCM matrix. "Zenith" in the body frame
-	double timeIncrement; 
-	
+	double timeIncrement;
 
-public: 
-	OrientationEstimator(MockIMU * i); 
-	void normalize(vector<double> &); 
+public:
+	OrientationEstimator(MockIMU * i);
+	void normalize(vector<double> &);
 	void orthonormalizeDCM();
-	vector<double> crossProduct(vector<double>, vector<double>); 
-	void updateDCM(); 
-	Quaternion getQuaternion(); 
+	vector<double> crossProduct(vector<double>, vector<double>);
+	void updateDCM();
+	Quaternion getQuaternion();
 	vector<double> accelerometerDeltaAngle();
 	vector<double> gyroscopeDeltaAngle();
 	vector<double> magnetDeltaAngle();
-}
+};
